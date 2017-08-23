3c03935d8aafe3a66944ee55b653f943473f43eb
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-08-23 23:37:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ var element = document.getElementById('name text');
 element.innerHTML = 'new value';
 
 //move  the image 
-var imd = document.getElementById('img');
+var imd = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginleft = '100px';
 };
