77407dc545d0c1fa521e6c9a044b421b6ddcdf00
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-08-23 22:54:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,7 +108,7 @@ return htmltemplate;
 }
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendfile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articlename', function (req, res) {
