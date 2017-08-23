2a7bea66a6803dcf586c5909ec0026594f754786
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-08-23 23:44:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,5 +7,5 @@ element.innerHTML = 'new value';
 //move  the image 
 var img = document.getElementById('madi');
 img.onclick = function(){
-    img.style.marginleft = '100px';
+    img.style.marginleft = '400px';
 };
