55798d9dde5bd0dd12197f9153657ed1656c519a
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-08-29 22:49:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -111,6 +111,12 @@ app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter',function(req,res){
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articlename', function (req, res) {
     //articlename = article-one
     //articles[articlename] = {}  content object for article-one
