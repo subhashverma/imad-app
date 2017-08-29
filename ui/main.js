ae3f3479c4433fa2e1434979b5e4549bf4b04231
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-08-30 00:08:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,24 @@
 // counter code
 var button = document.getElementById('counter');
-var counter = 0;
 button.onclick = function (){
-  // make a request to the counter end point
+  // create a request object
+  var request = new XMLHttpRequest();
   // capture the response and store it in a variable
-  // render the variable in the correct span
-  counter = counter + 1;
+  request.onreadystatechange = function (){
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // TAKE some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();
+              
+          }
+      }
+      // not done yet
+  };
   
-  var span = document.getElementById('count');
-  span.innerHTML = counter.toString();
+  // make the reqest
+  
+  request.open('GET','http://subhashverma446.imad.hasura-app.io/counter', true);
+  request.open(null);
 };
