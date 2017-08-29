7ec530cad723b1a3c8939a4b0e6bf461fbc26321
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-08-29 23:05:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,12 @@
-console.log('Loaded!');
-//change the text of name text
-var element = document.getElementById('name text');
-
-element.innerHTML = 'new value';
-
-//move  the image 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight()
-{
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft +'px';
-}
-img.onclick = function() 
-{
-    var interval = setInterval(moveRight, 50);
-    
+// counter code
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function (){
+  // make a request to the counter end point
+  // capture the response and store it in a variable
+  // render the variable in the correct span
+  counter = counter + 1;
+  
+  var span = document.getElementById('count');
+  span.innerHTML = counter.toString();
 };
