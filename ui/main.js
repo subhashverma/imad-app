ccdd46df7d25e16c904a939bfac7158faf8a1ef2
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-08-30 00:46:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,11 +34,11 @@ submit.onclick = function () {
   
   // capture a list and render a list
   
-  var names = ['name1','name2','name4'];
+  var names = ['name1','name2','name3','name4'];
   var list = '';
   for (var i=0; i< names.length; i++) {
       list <= '<li>' + names[i] + '</li>';
-  }
+  };
   var ul = document.getElementById('namelist');
   ul.innerHTML = list;
 };
