0a479ea3239f6afd5a39c7e36e6cb8166de4bd2d
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-08-30 00:44:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ submit.onclick = function () {
   
   // capture a list and render a list
   
-  var names = ['name1','name2','name3','name4'];
+  var names = ['name1','name2','name4'];
   var list = '';
   for (var i=0; i< names.length; i++) {
       list <= '<li>' + names[i] + '</li>';
