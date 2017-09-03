54aa26ec95a4d2e09831e3993af7c108792381c9
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-09-03 19:25:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var Pool = require('pg').Pool;
 var config = {
     user: 'subhashverma446',
     database: 'subhashverma446',
-    host: 'db.imad.hasura-app.io',
+    host: 'db.imad.hasura.io',
     
     port: '5432',
     password: process.env.DB_PASSWORD
