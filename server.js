c7b06ca7a1aa68c8f7862939cd7f9bdaeb2f341a
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-09-03 19:22:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+
+var config = {
+    user: 'subhashverma446',
+    database: 'subhashverma446',
+    host: 'db.imad.hasura-app.io',
+    
+    port: '5432',
+    password: process.env.DB_PASSWORD
+    
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -109,6 +121,19 @@ return htmltemplate;
 
 app.get('/', function (req, res) {
   res.sendfile(path.join(__dirname, 'ui', 'index.html'));
+});
+var pool = new Pool(config);
+
+app.get('/test-db',function(){
+    // make a select request
+    // return a response with the result
+     pool.query('SELECT * FROM test',function(err,res){
+       if(err)  {
+           res.status(500).send(err.toString());
+       }else
+       res.send(JSON.stringify(result));
+         
+     });
 });
 
 var counter = 0;
