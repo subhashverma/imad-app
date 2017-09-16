8a2926718e3b145f78df3f9bc3548b9c45f70807
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-09-16 17:47:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,7 +158,7 @@ app.get('/articles/:articleName', function (req, res) {
     //articles[articlename] = {}  content object for article-one
     
     
-    Pool.query("SELECT * FROM article WHERE title = " + req.params.articleName , function(err,result){
+    pool.query("SELECT * FROM article WHERE title = " + req.params.articleName, function(err,result){
        if (err){
            res.status(500).send(err.toString());
            } else
