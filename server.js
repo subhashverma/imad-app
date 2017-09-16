0652174e5ed5c24432d83886618deb3d4da65b52
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-09-16 16:38:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -166,7 +166,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/name', function (req,res){
+app.get('/submit-name/:name', function (req,res){
     // get the name from request object
     var name = req.params.name;
     
