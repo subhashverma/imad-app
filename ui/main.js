bd1c4ea67c648d0a34bd2b275ac1a3249404e913
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-09-16 16:57:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,7 @@ button.onclick = function () {
   
   // capture the response and store it in a variable
   request.onreadystatechange = function (){
+      
       if (request.readyState === XMLHttpRequest.DONE) {
           // TAKE some action
           if (request.status === 200) {
@@ -32,16 +33,41 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
-  // make a request to server and send the name
+    
+     // create a request object
+  var request = new XMLHttpRequest();
+  
+  // capture the response and store it in a variable
+  request.onreadystatechange = function (){
+      
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // TAKE some action
+          if (request.status === 200) {
+
   
   
   // capture a list and render a list
   
-  var names = ['name1','name2','name3','name4'];
+  var names = request.responseText;
+  names = JSON.parse(names);
   var list = '';
   for (var i=0; i< names.length; i++) {
       list += '<li>' + names[i] + '</li>';
 }
   var ul = document.getElementById('namelist');
   ul.innerHTML = list;
+              
+            
+              
+          }
+      }
+      // not done yet
+  };
+  // make the reqest
+  
+  request.open('GET','http://subhashverma446.imad.hasura-app.io/submit-name?name=' + name, true);
+  request.send(null);
+
+
+ 
 };
