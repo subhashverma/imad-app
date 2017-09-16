a4d741601f74ed5ef92681cba9fb00cea0a96bef
subhashverma <30776746+subhashverma@users.noreply.github.com>
2017-09-16 17:09:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,8 +28,7 @@ button.onclick = function () {
 };
 
 // submit name 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
@@ -64,6 +63,8 @@ submit.onclick = function () {
       // not done yet
   };
   // make the reqest
+  var nameInput = document.getElementById('name');
+var name = nameInput.value;
   
   request.open('GET','http://subhashverma446.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
