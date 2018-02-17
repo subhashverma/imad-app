33d34f3a91f4cb2d77352ea8b9d340eec103fbd0
subhash chander verma <30776746+subhashverma@users.noreply.github.com>
2018-02-17 21:30:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,12 +6,12 @@ button.onclick = function () {
    var img = document.getElementById('madi');
    var marginLeft = 0;
    function moveRight () {
-       marginLeft =marginLeft + 10;
+       marginLeft = marginLeft + 10;
        img.style.marginLeft = marginLeft + 'px';
    }
    img.onclick = function () {
        var interval = setInterval(moveRight, 100);
-       img.style.marginleft = '300px';
+       
        
    };
   // create a request object
