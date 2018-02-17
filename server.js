75f8eb768c083246cd7805c0720d42f7ea5386ad
subhash chander verma <30776746+subhashverma@users.noreply.github.com>
2018-02-17 17:14:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,6 +117,9 @@ function createTemplate (data){
     return htmlTemplate;
 }
 
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
 
 
 app.get('/:articleName', function (req, res) {
