00d452d26e5a795e7f7284ee60068caf6d927035
subhash chander verma <30776746+subhashverma@users.noreply.github.com>
2018-02-17 16:50:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -176,7 +176,7 @@ app.get('/articles/:articleName', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(createTemplate(articles));
+  res.send(createTemplate(articles/article-one));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
