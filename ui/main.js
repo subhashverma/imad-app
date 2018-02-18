04c1495ac3dfed700c7697597c4e6baf2a6a0dad
subhash chander verma <30776746+subhashverma@users.noreply.github.com>
2018-02-18 11:00:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,8 +37,7 @@ button.onclick = function () {
        var interval = setInterval(moveRight, 50);
 };
 // submit name 
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
@@ -71,7 +70,8 @@ submit.onclick = function () {
       // not done yet
   };
   // make the reqest
-
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
   
   request.open('GET','http://subhashverma446.imad.hasura-app.io/submit-name?name=' + name, true);
   request.send(null);
