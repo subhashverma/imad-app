2ecc77f31dc94b69c58d89d062cc76fbf20f66b4
subhash chander verma <30776746+subhashverma@users.noreply.github.com>
2018-02-24 23:40:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
  
-var articles = {
+/*var articles = {
     'article-one': {
         
         title: 'article one subhash verma',
@@ -75,7 +75,7 @@ var articles = {
       </p>`
         
         }
-};
+};*/
 function createTemplate (data) {
     
     var title = data.title;
