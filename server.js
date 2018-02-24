baed21de0699a4baafc1cb8a2de582c40a8de9cd
subhash chander verma <30776746+subhashverma@users.noreply.github.com>
2018-02-24 23:00:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.get('/articles/:articleName', function (req, res) {
               var articleData = result.rows[0];
               res.send(createTemplate(articleData));
           }
-      }
+      
    });
   
 });
