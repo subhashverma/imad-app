0d65a170efe1694fab2c9215d211aad8f6ccd297
subhash chander verma <30776746+subhashverma@users.noreply.github.com>
2018-02-24 23:00:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -153,10 +153,7 @@ app.get('/articles/:articleName', function (req, res) {
    pool.query("SELECT * FROM article WHERE title = ' " + req.params.articleName + " ' ", function(err, result){
       if (err) {
            res.status(500).send(err.toString());
-      } else {
-          if (result.rows.length == 0) {
-              res.status(404).send('Article not found');
-          } else {
+      } else  {
               var articleData = result.rows[0];
               res.send(createTemplate(articleData));
           }
