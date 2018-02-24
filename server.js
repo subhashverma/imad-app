8a0c592942817e675f9f23ac9e2cedf2f7fe71ca
subhash chander verma <30776746+subhashverma@users.noreply.github.com>
2018-02-24 20:56:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,7 +122,7 @@ app.get('/', function (req, res) {
 
 var pool =new Pool(config);
 app.get('/test-db', function(req, res){
-  Pool.query('SELECT * FROM test', function(err, result){
+  pool.query('SELECT * FROM test', function(err, result){
       if(err){
           res.status(500).send(err.toString());
       } else {
